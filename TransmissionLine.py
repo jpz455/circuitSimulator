5034e40baf558c187cbfa72688d5a54e100d4c57
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-01-28 09:02:38 -0500

    definition of class not completely done

---
+++ TransmissionLine.py
@@ -0,0 +1,24 @@
+class TransmissionLine:
+
+    def __init__(self,name:str,bus1:str,bus2:str,bundle:float,geometry:str,length:float):
+        self.name = name
+        self.bus1 = bus1
+        self.bus2 = bus2
+        self.bundle = bundle
+        self.geometry = geometry
+        self.length = length
+        self.series_impedance = self.calculate_series_impedance()
+        self.shunt_admittance = self.calculate_shunt_admittance()
+        self.y_admittance_matrix = self.calculate_y_matrix()
+
+
+    def calculate_series_impedance(self):
+
+    def calculate_shunt_admittance(self):
+
+    def calculate_y_matrix(self):
+
+
+
+
+
