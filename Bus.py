cc927472db22de9fbf94773500492d56aad2dd63
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-01-28 08:37:02 -0500

    definition of class

---
+++ Bus.py
@@ -0,0 +1,17 @@
+
+class Bus:
+    numBus=0
+    def __init__(self,name:str,base_kv:float):
+        self.name = name
+        self.base_kv = base_kv
+        Bus.numBus += 1  # Increment the class variable
+
+
+
+
+
+
+
+
+
+
