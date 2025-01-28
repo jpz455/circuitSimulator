e0b81238d67c1a1cf77cfb0d74749805fd86ea98
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-01-28 11:59:19 -0500

    Class Implementation Finished

--- Geometry.py
+++ Geometry.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Geometry:
     def __init__(self,name:str,xa:float,ya:float,xb:float,yb:float,xc:float,yc:float):
         self.name = name
@@ -11,3 +12,7 @@ class Geometry:
 
     def calculate_Deq(self):
         #cube root(Dab*Dbc*Dca)
+        self.Dab = np.sqrt((self.xa - self.xb) ** 2 + (self.ya - self.yb) ** 2)
+        self.Dbc = np.sqrt((self.xb - self.xc) ** 2 + (self.yb - self.yc) ** 2)
+        self.Dca = np.sqrt((self.xc - self.xa) ** 2 + (self.yc - self.ya) ** 2)
+        self.Deq = np.power((self.Dab * self.Dbc * self.Dca),1/3)
