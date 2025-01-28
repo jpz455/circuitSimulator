45a1a67f3aff55123638b6a29fe5a1068e69278a
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-01-28 12:13:51 -0500

    Class Implementation Finished

--- Conductor.py
+++ Conductor.py
@@ -1,9 +1,10 @@
 class Conductor:
     def __init__(self,name:str,diameter:float,GMR:float,resistance:float,ampacity:float):
         self.name = name
-        self.diameter = diameter
-        self.GMR = GMR
+        self.diameter = diameter #outside diameter in inches
+        self.GMR = GMR #at 60 Hz
         self.resistance = resistance
         self.ampacity = ampacity
+        self.radius = self.diameter/24 #converting diameter to radius in inches
 
 
