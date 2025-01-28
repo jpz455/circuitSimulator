2e34251ad4eb259365a9d76c41ca6daf595896c4
Allison M <anm332@pitt.edu>
2025-01-28 08:55:22 -0500

    transformer done except y matrix method

---
+++ Conductor.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Transformer():
+    def __init__(self, name: str, bus1: str, bus2: str, power_rating: float, impedance_percent: float, x_over_r_ratio: float):
+        self.name = name
+        self.bus1 = bus1
+        self.bus2 = bus2
+        self.power_rating = power_rating
+        self.impedance_percent = impedance_percent
+        self.x_over_r_ratio = x_over_r_ratio
+        self.z = self.calc_z()
+        self.y = self.calc_y()
+
+    def calc_z(self):
+        self.impedance_percent = self.impedance_percent * np.exp(1j * np.arctan(self.x_over_r_ratio))
+    def calc_y(self):
+        self.calc_z()
+        self.admittance = 1/self.impedance
