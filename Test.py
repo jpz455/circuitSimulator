3ca87e7b7de24702ca3c3f391d1d6e0ffb527a38
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-01-30 13:19:44 -0500

    Need Documentation

--- Test.py
+++ Test.py
@@ -1,11 +1,9 @@
-from setuptools import Require
-
-import Bundle as bundle
-import Bus as bus
-import TransmissionLine as line
-import Transformer as xfmr
+from Bundle import Bundle as Bundle
+from Conductor import Conductor as Conductor
+from Bus import Bus as Bus
+from Geometry import Geometry as Geometry
+from TransmissionLine import TransmissionLine as TransmissionLine
 import numpy as np
-
 from Transformer import Transformer
 
 #validate transformer
@@ -15,12 +13,37 @@ print("bus1: ", xfmr1.bus1, "; should be bus1")
 print("bus2: ", xfmr1.bus2, "; should be bus2")
 print("power rating: ", xfmr1.power_rating, "; should be 124")
 print("impedance percent: ", xfmr1.impedance_percent, "; should be 8.5")
+print("ratio: ", xfmr1.x_over_r_ratio, "; should be 10")
 
 xfmr1.calc_z()
 xfmr1.calc_y()
-print("z: ", xfmr1.z, "; should be ")
-print("y: ", xfmr1.y, "; should be ")
+print("z: ", xfmr1.z, "; should be ", 0.085*np.exp(1.47113*1j))
+print("y: ", xfmr1.y, "; should be ", 1/(0.085*np.exp(1.47113*1j)))
+print()
+
 
 xfmr1.calc_yprim()
 xfmr1.print_yprim()
 
+#**********CONDUCTOR VALIDATION******************
+conductor1 = Conductor("Partridge", 0.642, 0.0217,0.385, 460)
+print(conductor1.name, conductor1.diameter,conductor1.GMR, conductor1.resistance, conductor1.amp)
+#*********BUS VALIDATION**************************
+bus1 = (Bus("Bus 1", 20))
+bus2 = Bus("Bus 2", 230)
+print(bus1.name, bus1.base_kv, bus1.index)
+print(bus2.name, bus2.base_kv, bus2.index)
+#************BUNDLE VALIDATION**********************
+bundle1 = Bundle("Bundle 1", 2, 1.5, conductor1)
+print(bundle1.name, bundle1.num_conductors,bundle1.spacing, bundle1.conductor.name)
+print(bundle1.DSC, bundle1.DSL)
+#*********GEOMETRY VALIDATION***************
+geometry1 = Geometry("Geometry 1", 0, 0, 18.5, 0,37, 0)
+print(geometry1.name, geometry1.xa, geometry1.ya,geometry1.xb, geometry1.yb, geometry1.xc, geometry1.yc)
+print(geometry1.Deq)
+#*******************TRANSMISSION LINE VALIDATION************
+line1 = TransmissionLine("Line 1", bus1, bus2,bundle1, geometry1, 10)
+print(line1.name, line1.bus1.name, line1.bus2.name,line1.length)
+print(line1.z_base, line1.y_base)
+print(line1.Z, line1.B, line1.Y)
+print(line1.y_matrix)
