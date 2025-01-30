934f5cf654905b337f0f8d9c945a5270c0b6ca67
Allison M <anm332@pitt.edu>
2025-01-30 09:01:41 -0500

    validated transformer, fixed matrix printing to be ~ pretty ~

--- Test.py
+++ Test.py
@@ -2,7 +2,6 @@ from setuptools import Require
 
 import Bundle as bundle
 import Bus as bus
-import TransmissionLine as line
 import Transformer as xfmr
 import numpy as np
 
@@ -15,11 +14,14 @@ print("bus1: ", xfmr1.bus1, "; should be bus1")
 print("bus2: ", xfmr1.bus2, "; should be bus2")
 print("power rating: ", xfmr1.power_rating, "; should be 124")
 print("impedance percent: ", xfmr1.impedance_percent, "; should be 8.5")
+print("ratio: ", xfmr1.x_over_r_ratio, "; should be 10")
 
 xfmr1.calc_z()
 xfmr1.calc_y()
-print("z: ", xfmr1.z, "; should be ")
-print("y: ", xfmr1.y, "; should be ")
+print("z: ", xfmr1.z, "; should be ", 0.085*np.exp(1.47113*1j))
+print("y: ", xfmr1.y, "; should be ", 1/(0.085*np.exp(1.47113*1j)))
+print()
+
 
 xfmr1.calc_yprim()
 xfmr1.print_yprim()
