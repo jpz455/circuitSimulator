ec91ba1b7331ac6e17125560f23af76a7f2f722c
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-02-05 14:41:48 -0500

    Adding pu

--- TransmissionLine.py
+++ TransmissionLine.py
@@ -15,16 +15,21 @@ class TransmissionLine:
         self.bundle = bundle
         self.geometry = geometry
         self.length = length
+        self.calculate_base_values()
         self.calculate_series_impedance()
         self.calculate_admittance()
         self.calculate_y_matrix()
-        self.calculate_base_values()
+
 
     def calculate_series_impedance(self):
         self.R = self.bundle.resistance
         #X = 2pif * (2e-7*ln(Deq/Dsl)*1609
         self.X = 2*np.pi*current_settings.f*2*10e-7*np.log(self.geometry.Deq/self.bundle.DSL)*1609
         self.Z = (self.R*self.length) + 1j * (self.X*self.length)
+        self.Rpu = self.R/self.z_base
+        self.Xpu = self.X/self.z_base
+        self.Zpu = self.Z/self.z_base
+
 
 
     def calculate_admittance(self):
@@ -32,14 +37,22 @@ class TransmissionLine:
         self.B =1j*current_settings.f*((2*np.pi*np.power(8.85,-12))/(np.log(self.geometry.Deq/self.bundle.DSC))*1609)*self.length
         self.Y =1/self.Z
 
+        #Y (series) = 1/Z (series)
+        #Y (shunt) = G +jB
+        self.Bpu = self.B/self.y_base
+        self.Ypu = 1/self.Zpu
+
+        self.Ytotal = self.Ypu +self.Bpu
+
+
+
     def calculate_y_matrix(self):
-        y_prim = np.zeros((2,2),dtype=complex)
-        y_prim[0,0] = y_prim[1,1] = self.Y + self.B /2
-        y_prim[0,1] = y_prim[1,0] = -self.Y
+        prim_y = np.array([[self.Ytotal, -1 * self.Ypu], [-1 * self.Ypu, self.Ytotal]])
+        y_prim = pd.DataFrame(prim_y, [self.bus1.name, self.bus2.name], [self.bus1.name, self.bus2.name],dtype=complex)
         self.y_matrix = y_prim
         self.matrix = {
-            "y matrix": [y_prim[0,0], y_prim[0,1]],
-            "": [y_prim[1,0], y_prim[1,1]]
+            "y matrix": [prim_y[0,0], prim_y[0,1]],
+            "": [prim_y[1,0], prim_y[1,1]]
         }
 
 
