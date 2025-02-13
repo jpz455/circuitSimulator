967e9269366546bd0836cd52059c39ee4bebe435
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-02-13 08:41:58 -0500

    Cleaning up

--- TransmissionLine.py
+++ TransmissionLine.py
@@ -7,11 +7,10 @@ import pandas as pd
 
 class TransmissionLine:
 
-    def __init__(self,name:str,bus1: str, bus2: str, bus_dict: dict,bundle:Bundle,geometry:Geometry,length:float):
+    def __init__(self,name:str,bus1:Bus, bus2:Bus,bundle:Bundle,geometry:Geometry,length:float):
         self.name = name
         self.bus1 = bus1
         self.bus2 = bus2
-        self.bus_dict = bus_dict 
         self.bundle = bundle
         self.geometry = geometry
         self.length = length
@@ -19,9 +18,6 @@ class TransmissionLine:
         self.calculate_series_impedance()
         self.calculate_admittance()
         self.calc_yprim()
-
-    def get_bus(self, key: str):
-        return self.bus_dict[key]
 
     def calculate_series_impedance(self):
         self.R = self.bundle.resistance
@@ -34,29 +30,27 @@ class TransmissionLine:
 
     def calculate_admittance(self):
         #Y = 2pif*(2pi*eps*/ln(Deq/Dsc)*1609)
-        self.B =1j*current_settings.f*((2*np.pi*np.power(8.85,-12))/(np.log(self.geometry.Deq/self.bundle.DSC))*1609)*self.length
-        self.Y =1/self.Z
+        self.Yshunt =1j*current_settings.f*((2*np.pi*np.power(8.85,-12))/(np.log(self.geometry.Deq/self.bundle.DSC))*1609)*self.length
+        self.Yseries =1/self.Z
 
         #Y (series) = 1/Z (series)
         #Y (shunt) = G +jB
 
-        self.Bpu = self.B/self.y_base
-        self.Ypu = 1/self.Zpu
-        self.Ytotal = self.Ypu +self.Bpu/2
+        self.Yshuntpu = self.Yshunt/self.y_base
+        self.Yseriespu = 1/self.Zpu
 
     def calc_yprim(self):
-        yprim_matrix = np.array([[self.Ytotal, -1 * self.Ypu], [-1 * self.Ypu, self.Ytotal]])
+        yprim_matrix = np.array([[self.Yseriespu+.5*self.Yshuntpu, -1 * self.Yseriespu], [-1 * self.Yseriespu, self.Yseriespu+.5*self.Yshuntpu]])
         self.yprim = {
             "y matrix": [yprim_matrix[0,0], yprim_matrix[0,1]],
             "": [yprim_matrix[1,0], yprim_matrix[1,1]]
         }
         # Creating a Pandas DataFrame with bus names as labels
-        self.y_matrix_df = pd.DataFrame(yprim_matrix,index=[self.bus1, self.bus2],columns=[self.bus1, self.bus2])
+        self.y_matrix_df = pd.DataFrame(yprim_matrix,index=[self.bus1.name, self.bus2.name],columns=[self.bus1.name, self.bus2.name])
 
 
     def calculate_base_values(self):
-        bus1 = self.get_bus(self.bus1)  # Get Bus object dynamically
-        self.z_base = (bus1.base_kv ** 2) / current_settings.s_base
+        self.z_base = (self.bus1.base_kv ** 2) / current_settings.s_base
         self.y_base = 1 / self.z_base
 
     def print_yprim(self):
