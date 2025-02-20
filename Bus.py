31525a6d5ac8586a0b6ca31e7cda33a6324365a2
Allison M <anm332@pitt.edu>
2025-02-20 08:30:43 -0500

    new branch- bus is refactored

--- Bus.py
+++ Bus.py
@@ -1,11 +1,24 @@
+from numpy.f2py.auxfuncs import throw_error
+
+
 class Bus:
     numBus=0
-    def __init__(self,name:str,base_kv:float):
+    def __init__(self,name:str,base_kv:float, bus_type: str, vpu: float = 1.0, delta: float = 0.0):
         self.name = name
         self.base_kv = base_kv
         Bus.numBus += 1  # Increment the class variable
         self.index = Bus.numBus
+        self.bus_type = bus_type
+        self.vpu = vpu
+        self.delta = delta
 
+        self.set_bus_type() #validate bus_type
+
+
+    def set_bus_type(self):
+        self.bus_type_check = self.bus_type.lower()
+        if self.bus_type_check != "slack" or self.bus_type_check != "pq" or self.bus_type_check != "pv":
+            throw_error("Bus type must be Slack or PQ or PV")
 
 
 
