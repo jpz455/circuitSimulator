8f3bf268cbc7661608c22bac155e1f1ba825d5a7
Allison M <anm332@pitt.edu>
2025-03-18 08:20:40 -0400

    added calc methods

--- Jacobian.py
+++ Jacobian.py
@@ -8,6 +8,7 @@ class Jacobian:
     def __init__(self, solution: Solution):
         self.solution = solution
         self.circuit = solution.circuit
+        self.y_bus = np.array(self.solution.ybus) #convert y_bus to array
 
     def calc_jacobian(self, buses: np.array, ybus: pd.DataFrame, angles: np.array, voltages: np.array):
        #call helper methods to calculate each submatrix
@@ -26,37 +27,67 @@ class Jacobian:
     def calc_j1(self):
 
         j1 = np.array([14, 14])
-        # convert y_bus from dataframe to array
-        y_bus = np.array(self.solution.ybus)
         # now extract Vk, Vkn, Vn
         for k, bus_k in enumerate(self.circuit.buses.values()):
             for n, bus_n in enumerate(self.circuit.buses.values()):
-                v_k = bus_k.v_pu
-                v_n = bus_n.v_pu
-                y_kn = np.abs(y_bus[k, n])
-                delta_k = bus_k.delta*np.pi/180
-                delta_n = bus_n.delta*np.pi/180
-                theta_kn = np.angle(y_bus[k, n])
+                if(k != n):
+                    v_k = bus_k.v_pu #get Vk
+                    v_n = bus_n.v_pu #get Vn
+                    y_kn = np.abs(self.y_bus[k, n]) #get Ykn
+                    delta_k = bus_k.delta*np.pi/180 #get delta angle for bus k
+                    delta_n = bus_n.delta*np.pi/180 #get delta angle for bus n
+                    theta_kn = np.angle(self.y_bus[k, n]) #get theta for kn from Y_bus
 
-                #add to matrix
-                j1[k, n] = v_k*y_kn*v_n*np.sin(delta_k - delta_n - theta_kn)
-
+                    #add to matrix using partial derivative equation
+                    j1[k, n] = v_k*y_kn*v_n*np.sin(delta_k - delta_n - theta_kn) #solve equation for partial derivative and add
         return j1
 
     def calc_j2(self):
-        # to do
         j2 = np.array([14, 14])
+        # now extract Vk, Vkn, Vn
+        for k, bus_k in enumerate(self.circuit.buses.values()):
+            for n, bus_n in enumerate(self.circuit.buses.values()):
+                v_k = bus_k.v_pu  # get Vk
+                y_kn = np.abs(self.y_bus[k, n])  # get Ykn
+                delta_k = bus_k.delta * np.pi / 180  # get delta angle for bus k
+                delta_n = bus_n.delta * np.pi / 180  # get delta angle for bus n
+                theta_kn = np.angle(self.y_bus[k, n])  # get theta for kn from Y_bus
+
+                # add to matrix using partial derivative equation
+                j2[k, n] = v_k * y_kn* np.cos(delta_k - delta_n - theta_kn)  # solve equation for partial derivative and add
+
         return j2
 
-    def calc_j2(self):
-        # to do
-        j2 = np.array([14, 14])
-        return j2
+    def calc_j3(self):
+        j3 = np.array([14, 14])
+        # now extract Vk, Vkn, Vn
+        for k, bus_k in enumerate(self.circuit.buses.values()):
+            for n, bus_n in enumerate(self.circuit.buses.values()):
+                v_k = bus_k.v_pu  # get Vk
+                v_n = bus_n.v_pu  # get Vn
+                y_kn = np.abs(self.y_bus[k, n])  # get Ykn
+                delta_k = bus_k.delta * np.pi / 180  # get delta angle for bus k
+                delta_n = bus_n.delta * np.pi / 180  # get delta angle for bus n
+                theta_kn = np.angle(self.y_bus[k, n])  # get theta for kn from Y_bus
 
-    def calc_j2(self):
-        # to do
-        j2 = np.array([14, 14])
-        return j2
+                # add to matrix using partial derivative equation
+                j3[k, n] = -1 * v_k * y_kn * v_n * np.cos(delta_k - delta_n - theta_kn)  # solve equation for partial derivative and add
+        return j3
+
+    def calc_j4(self):
+        j4 = np.array([14, 14])
+        # now extract Vk, Vkn, Vn
+        for k, bus_k in enumerate(self.circuit.buses.values()):
+            for n, bus_n in enumerate(self.circuit.buses.values()):
+                v_k = bus_k.v_pu  # get Vk
+                y_kn = np.abs(self.y_bus[k, n])  # get Ykn
+                delta_k = bus_k.delta * np.pi / 180  # get delta angle for bus k
+                delta_n = bus_n.delta * np.pi / 180  # get delta angle for bus n
+                theta_kn = np.angle(self.y_bus[k, n])  # get theta for kn from Y_bus
+
+                # add to matrix using partial derivative equation
+                j4[k, n] = v_k * y_kn * np.sin(delta_k - delta_n - theta_kn)  # solve equation for partial derivative and add
+        return j4
 
     def calc_off_diag(self):
         #helper method to get inputs for matrix for an iteration
