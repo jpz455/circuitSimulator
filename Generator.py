8cfa1a7bf6d799ae8643adc80983dbe0cf52ecd9
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-04-07 21:15:55 -0400

    edited Fault for subtransient

--- Generator.py
+++ Generator.py
@@ -1,20 +1,15 @@
 from Bus import Bus
-from Settings import Settings, current_settings
-
-
+from Settings import Settings
 class Generator:
-    def __init__(self,name:str,bus:Bus,voltage_setpoint:float,mw_setpoint:float, settings: Settings, base: float = 100):
+    def __init__(self,name:str,bus:Bus,voltage_setpoint:float,mw_setpoint:float, x1:float,x2:float,x0:float,settings: Settings):
         self.name = name
         self.voltage_setpoint = voltage_setpoint #in pu
         self.mw_setpoint = mw_setpoint #in MW
         self.bus = bus
         self.settings: Settings = settings
-        self.subtransient_x: complex = 0 + 0*1j
-        self.base = 100
-
-    def set_subtransient_x(self, subtransient_x: float):
-        self.subtransient_x = subtransient_x * (current_settings.s_base/self.base)
-
+        self.x1 = x1
+        self.x2 = x2
+        self.x0 = x0
 
 
 
