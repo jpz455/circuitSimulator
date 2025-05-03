31d32f389f153bd1322ddbffe237439030212706
Allison M <anm332@pitt.edu>
2025-05-02 20:27:08 -0400

    test

--- Test.py
+++ Test.py
@@ -8,104 +8,18 @@ import numpy as np
 from Transformer import Transformer
 from Circuit import Circuit as Circuit
 
-
-
-#************TRANSFORMER VALIDATION***********************#
-#
-# bus1 = Bus("Bus 1", 20, "Slack")
-# bus2 = Bus("Bus 2", 230, "PQ")
-# xfmr1 = Transformer("T1", bus1, bus2, 124, 8.5, 10)
-# print("Transformer Validation")
-# print("name: ", xfmr1.name, "; should be T1")
-# print("bus1: ", xfmr1.bus1, "; should be Bus 1")
-# print("bus2: ", xfmr1.bus2, "; should be Bus 2")
-# print("power rating: ", xfmr1.power_rating, "; should be 124")
-# print("impedance percent: ", xfmr1.impedance_percent, "; should be 8.5")
-# print("ratio: ", xfmr1.x_over_r_ratio, "; should be 10")
-# #**********CONDUCTOR VALIDATION******************
-# conductor1 = Conductor("Partridge", 0.642, 0.0217,0.385, 460)
-# print()
-# print("Conductor Validation")
-# print("name:", conductor1.name, ";should be Partridge")
-# print("diameter:", conductor1.diameter, ";should be 0.642")
-# print("GMR:", conductor1.GMR, ";should be 0.0217")
-# print("resistance:", conductor1.resistance, ";should be 0.385")
-# print("amperage:", conductor1.amp, ";should be 460")
-# print()
-
-
-#*********BUS VALIDATION**************************
-
-print("Bus Validation")
-bus1 = Bus("Bus 1", 20, "Slack")
-bus2 = Bus("Bus 2", 230, "PQ")
-print("bus1 name:", bus1.name, "; should be Bus 1")
-print("bus1 base voltage:", bus1.base_kv, ";should be 20")
-print("bus1 index:", bus1.index, "; should be 1")
-print("bus1 type:", bus1.bus_type, "; should be Slack")
-print("bus1 vpu:", bus1.v_pu, "; should be 1.0")
-print("bus1 delta:", bus1.delta, "; should be 0.0")
-
-
-print("bus2 name:", bus2.name, "; should be Bus 2")
-print("bus 2 base voltage:", bus2.base_kv, ";should be 230")
-print("bus 2 index: ", bus2.index, "; should be 2")
-print("bus2 type:", bus2.bus_type, "; should be PQ")
-print("bus2 vpu:", bus2.v_pu, "; should be 1.0")
-print("bus2 delta:", bus2.delta, "; should be 0.0")
-print()
-
-"""""
-#************BUNDLE VALIDATION**********************
-print("Bundle Validation")
-bundle1 = Bundle("Bundle 1", 2, 1.5, conductor1)
-print(bundle1.name, bundle1.num_conductors,bundle1.spacing, bundle1.conductor.name)
-print("bundle1 name:", bundle1.name, "; should be Bundle 1")
-print("bundle1 num conductors:", bundle1.num_conductors, "; should be 2")
-print("bundle1 spacing:", bundle1.spacing, "; should be 1.5")
-print("bundle1 DSC:", bundle1.DSC)
-print("bundle1 DSL:", bundle1.DSL)
-print()
-#*********GEOMETRY VALIDATION***************
-geometry1 = Geometry("Geometry 1", 0, 0, 18.5, 0,37, 0)
-print("Geometry Validation")
-print("Geometry 1 name:", geometry1.name, "; should be Geometry 1")
-print("Geometry 1 xa:", geometry1.xa, "; should be 0")
-print("Geometry 1 ya:", geometry1.ya, "; should be 0")
-print("Geometry 1 xb:", geometry1.xb, "; should be 18.5")
-print("Geometry 1 yb:", geometry1.yb, "; should be 0")
-print("Geometry 1 xc:", geometry1.xc, "; should be 37")
-print("Geometry 1 yc:", geometry1.yc, "; should be 0")
-print("Geometry 1 Deq:", geometry1.Deq)
-print()
-#*******************TRANSMISSION LINE VALIDATION************
-line1 = TransmissionLine("Line 1", bus1, bus2,bundle1, geometry1, 10)
-print("TransmissionLine Validation")
-print("Line 1 name:", line1.name, "; should be Line 1")
-print("Line 1 length:", line1.length, "; should be 10")
-print("Line 1 Z:", line1.Zpu)
-print("Line 1 Y:", line1.Ypu)
-print("Line 1 B:", line1.Bpu)
-line1.print_yprim()
-print()
-"""""
-#************************CIRCUIT VALIDATION****************************#
 # Create circuit instance
 circuit1 = Circuit("Test Circuit", current_settings)
 
-print(circuit1.name)  # Expected output: "Test Circuit"
-print(type(circuit1.name))  # Expected output: <class 'str'>
-print(circuit1.buses)  # Expected output: {}
-print(type(circuit1.buses))  # Expected output: <class 'dict'>
-
-# Correct way to add a bus
+# add bus
 bus1 = Bus("Bus1", 20, "Slack")
-bus2 = Bus("Bus2", 20, "PQ")
-bus3 = Bus('Bus3', 20, "PQ")
-bus4 = Bus("Bus4", 20, "PQ")
-bus5 = Bus("Bus5", 20, "PQ")
-bus6 = Bus("Bus6", 20, "PQ")
-bus7 = Bus("Bus7", 20, "PV")
+bus2 = Bus("Bus2", 230, "PQ")
+bus3 = Bus('Bus3', 230, "PQ")
+bus4 = Bus("Bus4", 230, "PQ")
+bus5 = Bus("Bus5", 230, "PQ")
+bus6 = Bus("Bus6", 230, "PQ")
+bus7 = Bus("Bus7", 18, "PV")
+
 circuit1.add_bus(bus1)
 circuit1.add_bus(bus2)
 circuit1.add_bus(bus3)
